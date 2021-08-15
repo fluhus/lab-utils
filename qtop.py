f005e746394b87d34289442711bf96d28cbadcb6
Amit Lavon <amit.lavon@weizmann.ac.il>
2021-08-15 10:22:25 +0300

    Make qtop use xml

--- qtop.py
+++ qtop.py
@@ -1,91 +1,71 @@
-import re
 import sys
+import xml.dom.minidom as xml
 from collections import defaultdict
 from math import log
 from subprocess import PIPE, Popen
-from typing import IO, List
+from typing import IO, List, Tuple
 
-ITEM_RE = re.compile('^\\s*\\d')
-NAME_RE = re.compile('^\\s*(\\S+\\s+){3}(\\S+)')
-STATUS_RE = re.compile('^\\s*(\\S+\\s+){4}(\\S+)')
-MEM_RE = re.compile('mem_free=(\\d+)(.)')
-CPU_RE = re.compile('\\sthreads (\\d+)\\s')
-
-MEM_SIGNS = {'M': 10**6, 'm': 10**6, 'G': 10**9, 'g': 10**9}
+MEM_SIGNS = {' ': 10**6, 'M': 10**6, 'm': 10**6, 'G': 10**9, 'g': 10**9}
 EPS = sys.float_info.epsilon
-
-
-def parse_item(lines: List):
-    cat = ' '.join(lines)
-    return extract_user(cat), 1, extract_cpu(cat), extract_mem(
-        cat), extract_status(cat)
-
-
-def extract_user(s: str) -> str:
-    m = NAME_RE.match(s)
-    if not m:
-        raise ValueError('Could not find username: ' + s)
-    return m.group(2)
-
-
-def extract_status(s: str) -> str:
-    m = STATUS_RE.match(s)
-    if not m:
-        raise ValueError('Could not find status: ' + s)
-    return m.group(2)
-
-
-def extract_mem(s: str) -> int:
-    m = MEM_RE.findall(s)
-    if not m:
-        return 0
-    return int(m[0][0]) * MEM_SIGNS[m[0][1]]
-
-
-def extract_cpu(s: str) -> int:
-    m = CPU_RE.findall(s)
-    if not m:
-        return 0
-    return int(m[0][0])
-
-
-def parse_items(f: IO):
-    # Skip header.
-    next(f)
-    next(f)
-
-    current = []
-    for line in f:
-        if not ITEM_RE.match(line):
-            assert current, f'Bad line (expected item header): {line}'
-            current.append(line.strip())
-            continue
-        if current:
-            yield parse_item(current)
-        current = [line.strip()]
-    if current:
-        yield parse_item(current)
 
 
 def tupsum(t1, t2):
     return tuple(t1[i] + t2[i] for i in range(len(t1)))
 
 
-def collect_items(f: IO):
+def job_stats(f: IO):
+    jobs = parse_jobs(f)
     d = defaultdict(lambda: (0, 0, 0))
-    for item in parse_items(f):
-        if item[-1] != 'r':
-            continue
-        item = item[:-1]
-        d[item[0]] = tupsum(d[item[0]], item[1:])
+    for job in jobs:
+        d[job[0]] = tupsum(d[job[0]], job[1:])
     return d
 
 
+def parse_jobs(f: IO) -> List[Tuple]:
+    with xml.parse(f) as dom:
+        dom: xml.Document
+        jobs = dom.getElementsByTagName('job_list')
+        jobs = [
+            job for job in jobs if job_name(job) != 'QRLOGIN' and job_is_running(job)
+        ]
+        return [(job_owner(j), 1, job_cpu(j), job_mem(j)) for j in jobs]
+
+
+def node_text(node: xml.Element, name: str) -> str:
+    subnode = node.getElementsByTagName(name)
+    assert len(subnode) == 1, f'found {len(subnode)} nodes named "{name}", want 1'
+    text = subnode[0].childNodes
+    assert len(text) == 1, f'found {len(text)} text nodes, want 1'
+    return text[0].data
+
+
+def job_is_running(job: xml.Element):
+    return node_text(job, 'state') == 'r'
+
+
+def job_owner(job: xml.Element) -> str:
+    return node_text(job, 'JB_owner')
+
+
+def job_name(job: xml.Element) -> str:
+    return node_text(job, 'JB_name')
+
+
+def job_cpu(job: xml.Element) -> str:
+    return int(node_text(job, 'requested_pe'))
+
+
+def job_mem(job: xml.Element) -> str:
+    raw = node_text(job, 'hard_request')
+    if '0' <= raw[-1] <= '9':  # Default is megabytes.
+        raw += 'M'
+    return int(raw[:-1]) * MEM_SIGNS[raw[-1]]
+
+
 def main():
-    cmd = Popen(['qstat', '-r', '-u', '*'], text=True, stdout=PIPE)
-    col = collect_items(cmd.stdout)
-    names = sorted(col.keys(),
-                   key=lambda k: -sum(log(x + EPS) for x in col[k]))
+    cmd = Popen(['qstat', '-xml', '-r', '-u', '*'], text=True, stdout=PIPE)
+    col = job_stats(cmd.stdout)
+    names = sorted(col.keys(), key=lambda k: -sum(log(x + EPS) for x in col[k]))
 
     print('User\tJobs\tCPU\tMem')
     for name in names[:5]:
